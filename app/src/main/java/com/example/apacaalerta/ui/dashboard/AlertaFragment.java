509e282eb2af577c9753de901be7be4401aa4494
DIOGOBRODRIGUES <diogo.borbar@gmail.com>
2022-11-27 01:59:40 -0300

    Implemntação do front Home/Alerta/Marés/Rios

--- app/src/main/java/com/example/apacaalerta/ui/dashboard/AlertaFragment.java
+++ app/src/main/java/com/example/apacaalerta/ui/dashboard/AlertaFragment.java
@@ -4,24 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.apacaalerta.databinding.FragmentDashboardBinding;
+import com.example.apacaalerta.databinding.FragmentAlertaBinding;
 
-public class DashboardFragment extends Fragment {
+public class AlertaFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentAlertaBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        AlertaViewModel alertaViewModel =
+                new ViewModelProvider(this).get(AlertaViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+       binding = FragmentAlertaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
 //        final TextView textView = binding.textDashboard;
